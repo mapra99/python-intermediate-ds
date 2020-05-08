9b9922b8662981667158d51fc858d227f7bc13c8
Miguel Prada <mapra99@gmail.com>
2020-05-08 00:31:37 -0500

    finished dicts

--- dictionaries.py
+++ dictionaries.py
@@ -12,3 +12,31 @@ europe = { 'spain': 'madrid',
 print(europe)
 print(europe.keys())
 print(europe['norway'])
+
+europe['italy'] = 'rome'
+print('italy' in europe)
+
+europe['poland'] = 'warsaw'
+print(europe)
+
+europe = {'spain':'madrid', 'france':'paris', 'germany':'bonn',
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw',
+          'australia':'vienna' }
+
+europe['germany'] = 'berlin'
+del(europe['australia'])
+
+# Dictionary of dictionaries
+europe = { 'spain': { 'capital':'madrid', 'population':46.77 },
+           'france': { 'capital':'paris', 'population':66.03 },
+           'germany': { 'capital':'berlin', 'population':80.62 },
+           'norway': { 'capital':'oslo', 'population':5.084 } }
+
+print(europe['france']['capital'])
+
+data = { 'capital': 'rome',
+         'population': 59.83 }
+
+europe['italy'] = data
+
+print(europe)
