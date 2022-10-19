a57aa7f4943a0f17c465872fd78b53fcf19dc569
Miguel Prada <mapra99@gmail.com>
2022-10-18 20:50:54 -0500

    merge old notes and new notes

--- dicts&pandas/.ipynb_checkpoints/csv_to_df-checkpoint.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-csv_path = "https://assets.datacamp.com/production/repositories/287/datasets/79b3c22c47a2f45a800c62cae39035ff2ea4e609/cars.csv"
-cars = pd.read_csv(csv_path, index_col=0)
-
-print(cars)
